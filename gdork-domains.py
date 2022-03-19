58693fce818cbe18b4c69b92297bb7533b5aba32
KickAss101 <jaswanthsunkara@protonamil.com>
2022-03-19 10:00:53 +0530

    refactor

--- gdork-domains.py
+++ gdork-domains.py
@@ -15,36 +15,37 @@ def main():
     parser.add_argument("-f","--file",help="provide file with list of dorks")
     parser.add_argument("-d","--dork",help="provide single dork")
     parser.add_argument("-n","--no-domains",help="Number of domains to enumerate (optional)",type=int,default=50)
-    parser.add_argument("-t","--tld",help="google TLD (optional)",type=str,default="com")
+    parser.add_argument("-t","--tld",help="google TLD (optional)",default="com")
     args = parser.parse_args()
 
     if args.file is None and args.dork is None:
         parser.print_help()
         exit()
 
-    domains = open('./result.txt', 'w')
+    domains = open('./result.txt', 'a')
 
     # if dorks file 
     if args.file is not None:
         dorks = open(args.file, 'r')
 
-        # Get all domains
+        # Get domains for dorks file
         for dork in dorks:
-            print(dork)
             scrape(dork, args, domains)
+        dorks.close()
 
     # if single dork is given
     else:
-        scrape(dork, args, domains)
+        scrape(args.dork, args, domains)
         
     domains.close()
-    dorks.close()
+
 
 # Scrape domains from google results
 def scrape(dork, args, domains):
+    print(dork)
     for url in search(dork, tld=args.tld, lang="en",num=args.no_domains, start=0, stop=None, pause=2):
         ext = tldextract.extract(url)
-        domain = f'{ext.domain}.{ext.suffix}\n'
+        domain = f'{ext.subdomain}.{ext.domain}.{ext.suffix}\n'
         print(domain)
         domains.write(domain)
 
